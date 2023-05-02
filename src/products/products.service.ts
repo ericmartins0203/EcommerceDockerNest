0a68e94500f4b1b8a0a3ce492038617eea95f7ec
ericmartins0203 <estevesmartinseric@gmail.com>
2023-05-02 14:19:25 -0300

    aplication with category and products using nest and docker

---
+++ src/products/products.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(CreateProductDto: CreateProductDto) {
+    return this.prisma.product.create({
+      data: CreateProductDto
+    });
+  }
+
+  findAll() {
+    return this.prisma.product.findMany({
+      orderBy: {
+        createdAt: 'desc'
+      }
+    });
+  }
+
+  findOne(id: number) {
+    return this.prisma.product.findFirstOrThrow({
+      where: {
+        id,
+      },
+    });
+  }
+
+  update(id: number, UpdateProductDto: UpdateProductDto) {
+    return this.prisma.product.update({
+      where: { 
+        id, 
+      },
+      data: UpdateProductDto
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.product.delete({
+      where: {
+        id,
+      }
+    });
+  }
+}
